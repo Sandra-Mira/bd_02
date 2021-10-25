c597d701bc5f7d3b26b4fd7dbba3be9079a2e30f
Sandra Mira <sandramiram1@gmail.com>
2021-10-25 13:08:08 +0200

    consulta de datos

--- squema.sql
+++ squema.sql
@@ -1,3 +1,5 @@
 SELECT*FROM sustantivos WHERE id_adjetivo=1 AND nombre LIKE "%JOSE%";
 select(select nombre from sustantivos)as nombres, (select nombre from adjetivos)as adjetivos;
 select s.id, s.nombre, ad.nombre as adjetivo from sustantivos as s JOIN adjetivos as ad ON(ad.id =s.id_adjetivo);
+INSERT INTO adjetivos(id,nombre) VALUES("6","frio");
+INSERT INTO adjetivos(id,nombre) VALUES("7","Amargo");
