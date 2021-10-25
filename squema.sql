385a8e61399754e8172be8ac2853b6a36cce178f
Sandra Mira <sandramiram1@gmail.com>
2021-10-25 12:47:57 +0200

    gato

---
+++ squema.sql
@@ -0,0 +1,3 @@
+SELECT*FROM sustantivos WHERE id_adjetivo=1 AND nombre LIKE "%JOSE%";
+select(select nombre from sustantivos)as nombres, (select nombre from adjetivos)as adjetivos;
+select s.id, s.nombre, ad.nombre as adjetivo from sustantivos as s JOIN adjetivos as ad ON(ad.id =s.id_adjetivo);
