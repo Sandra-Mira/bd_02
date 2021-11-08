c48ee319a99c0216a0ef9a8560fb1aa74d97f201
Sandra Mira <sandramiram1@gmail.com>
2021-11-08 10:19:00 +0100

    perro

--- squema.sql
+++ squema.sql
@@ -1,24 +1,24 @@
-SELECT*FROM sustantivos WHERE id_adjetivo=1 AND nombre LIKE "%JOSE%";
-select(select nombre from sustantivos)as nombres, (select nombre from adjetivo)as adjetivos;
-select s.id, s.nombre, ad.nombre as adjetivo from sustantivos as s JOIN adjetivo as ad ON(ad.id =s.id_adjetivo);
-INSERT INTO adjetivo(id,nombre) VALUES("6","frio");
-INSERT INTO adjetivo(id,nombre) VALUES("7","Amargo");
+SELECT*FROM sustantivos WHERE id_adjetivo=1 AND nombre LIKE "%JOSE%"
+select(select nombre from sustantivos)as nombres, (select nombre from adjetivo)as adjetivos
+select s.id, s.nombre, ad.nombre as adjetivo from sustantivos as s JOIN adjetivo as ad ON(ad.id =s.id_adjetivo)
+INSERT INTO adjetivo(id,nombre) VALUES("6","frio")
+INSERT INTO adjetivo(id,nombre) VALUES("7","Amargo")
 SELECT * FROM sustantivos WHERE id_adjetivo= 1;
 
-SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre= "Jose";
-SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre LIKE "%Jose%";
-SELECT *(select nombre from sustantivos) as nombre, (select nombre from adjetivo) as adjetivo;
+SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre= "Jose"
+SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre LIKE "%Jose%"
+SELECT *(select nombre from sustantivos) as nombre, (select nombre from adjetivo) as adjetivo
 
 --Para cojer un adjetivo de una fila en específico y un nombre de otra
 
-SELECT (select nombre from sustantivos where id=3) as nombre, (select nombre from adjetivo where id=4) as adjetivo;
+SELECT (select nombre from sustantivos where id=3) as nombre, (select nombre from adjetivo where id=4) as adjetivo
 
 --Para mostrar datos de 2 columnas diferentes, el as se utiliza para atribuirle un valor a las tablas y acortar las consultas
 
 SELECT sustantivos.nombre,adjetivo.nombre as adejtivo from sustantivos 
 JOIN adjetivos as ad ON s.id_adjetivo = ad.id 
-JOIN adjetivos.nombre="Jose"
+JOIN adjetivos.nombre="Jose";
 
-select sustantivos.nombre as nombre, adjetivo.nombre as adjetivo from sustantivos;
+select sustantivos.nombre as nombre, adjetivo.nombre as adjetivo from sustantivos
 JOIN adjetivo ON sustantivos.id_adjetivo= adjetivo.id;
 WHERE sustantivos.id =1;
