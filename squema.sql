63fe67b055f9ba1d3e3855c18c600e2c50f06b25
Sandra Mira <sandramiram1@gmail.com>
2021-11-08 09:50:26 +0100

    perro

--- squema.sql
+++ squema.sql
@@ -1,5 +1,14 @@
-SELECT*FROM sustantivos WHERE id_adjetivo=1 AND nombre LIKE "%JOSE%";
-select(select nombre from sustantivos)as nombres, (select nombre from adjetivos)as adjetivos;
-select s.id, s.nombre, ad.nombre as adjetivo from sustantivos as s JOIN adjetivos as ad ON(ad.id =s.id_adjetivo);
-INSERT INTO adjetivos(id,nombre) VALUES("6","frio");
-INSERT INTO adjetivos(id,nombre) VALUES("7","Amargo");
+
+SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre= "Jose"
+SELECT * FROM sustantivos WHERE id_adjetivo= 1 AND nombre LIKE "%Jose%"
+SELECT (select nombre from sustantivos) as nombre, (select nombre from adjetivos) as adjetivo
+
+--Para cojer un adjetivo de una fila en específico y un nombre de otra
+
+SELECT (select nombre from sustantivos where id=3) as nombre, (select nombre from adjetivos where id=4) as adjetivo
+
+--Para mostrar datos de 2 columnas diferentes, el as se utiliza para atribuirle un valor a las tablas y acortar las consultas
+
+SELECT sustantivos.nombre,adjetivos.nombre as adejtivo from sustantivos 
+JOIN adjetivos as ad ON (s.id_adjetivo = ad.id )
+JOIN adjetivos.nombre="Jose";
